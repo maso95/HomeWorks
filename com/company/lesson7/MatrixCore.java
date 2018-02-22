f9038803e4f5fa92cd4a761853ad3980373ce969
Андрей <maso@ukr.net>
2018-02-22 16:46:52 +0200

    зделал Матрицу и Readers(даные читателей+книги ),Book(класс книг),Library(класс читателй+пергруженые методы takeBook,returnBook)вроде все работает.

--- com/company/lesson7/MatrixCore.java
+++ com/company/lesson7/MatrixCore.java
@@ -3,14 +3,36 @@ package com.company.lesson7;
 import java.util.Arrays;
 
 public class MatrixCore {
-    int[][] mc = new int[][]{};
-    void addition(MatrixCore o) {
-        System.out.println();
-    }
+    int str, col;
+    int[][] array = new int[str][col];
 
-    public void addition() {
+
+    MatrixCore(int[][]array) {
+        this.array = array;
+        this.str = array.length;
+        this.col = array[0].length;
+    }
+    MatrixCore plus(MatrixCore m) {
+        int[][] array =  new int[str][col];
+        for (int i = 0; i < m.array.length; i++) {
+            for (int j = 0; j < m.array.length; j++) {
+                array[i][j] = this.array[i][j] + m.array[i][j];
+            }
+        }
+        MatrixCore s = new MatrixCore(array);
+        System.out.println(Arrays.deepToString(array));
+        return s;
+    }
+    MatrixCore multiple(int num) {
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                array[i][j] = array[i][j] * num;
+            }
+        }
+        MatrixCore s = new MatrixCore(array);
+        System.out.println(Arrays.deepToString(array));
+        return s;
     }
 }
-
 
 
